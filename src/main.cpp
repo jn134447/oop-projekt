c2dcbc73cd45eb15cb031374b83dbabc62652758
jn134447 <196645821+jn134447@users.noreply.github.com>
2025-12-11 13:04:21 +0100

    added external library raygui

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,52 @@
 #include <raylib-cpp.hpp>
 
-int main() {
-    
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+constexpr unsigned int FPS = 60;
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
     // Initialization
-    int screenWidth = 800;
-    int screenHeight = 450;
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    raylib::Color textColor(LIGHTGRAY);
-    raylib::Window w(screenWidth, screenHeight, "Raylib C++ Starter Kit Example");
-    SetTargetFPS(60);
+    raylib::Window w(screenWidth, screenHeight, "raylib [text] example - writing anim");
 
-    // Main game loop
+    raylib::Text msg("actually craaazy half my job done this shit is doable\n"
+                     "in 4 weeks bla bla bla bla bla bla bla bla\n"
+                     "bla bla bla bla bla bla bla bla bla bla\n",
+                     20, MAROON);
+
+    int framesCounter = 0;
+    SetTargetFPS(FPS); // Set our game to run at 60 frames-per-second
+
     while (!w.ShouldClose()) // Detect window close button or ESC key
     {
-        // Update
-
-        // TODO: Update your variables here
-
         // Draw
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        textColor.DrawText("Congrats! You created your first window!", 190, 200, 20);
-        EndDrawing();
-    }
+        //----------------------------------------------------------------------------------
+        w.BeginDrawing();
 
-    return 0;
+        w.ClearBackground(RAYWHITE);
+
+        if (GuiButton((Rectangle){24, 24, 120, 30}, "#191#speedup"))
+        {
+            framesCounter += 30;
+        }
+        else
+        {
+            framesCounter++;
+        }
+        if (GuiButton((Rectangle){24, 24 + 10 + 24, 120, 30}, "#191#restart"))
+        {
+            framesCounter = 0;
+        }
+
+        raylib::Text::Draw(msg.text.substr(0, framesCounter / 10), 210, 160, msg.GetFontSize(), msg.GetColor());
+
+        w.EndDrawing();
+    }
 }
